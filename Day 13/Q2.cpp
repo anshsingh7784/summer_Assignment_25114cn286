24c61f461c8827286a956648bfa5158f08956ae3
Ansh Singh <anshsingh7784@gmail.com>
2026-06-13 22:01:57 +0530

    Day 13 Assignment.

---
+++ Day 13/Q2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    cout<<"enter the size of the array: ";
+    int n;
+    cin>>n;
+    int sum=0;
+    vector<int> a;
+    cout<<"enter the elements of the array: ";
+    for(int i=0;i<n;i++)
+    {
+        int temp;
+        cin>>temp;
+        a.push_back(temp);
+        sum+=temp;
+    }
+    cout<<"the array elements are: "<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";    
+    }
+    cout<<endl;
+    cout<<"the sum of the array elements is: "<<sum<<endl;
+    return 0;
+}
