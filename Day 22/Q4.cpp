22cc3665e41ed4403e575abefb8f40addcba7090
Ansh Singh <anshsingh7784@gmail.com>
2026-06-21 21:48:56 +0530

    Day 22 Assignment.

---
+++ Day 22/Q4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+#include<cctype>
+using namespace std;
+int main()
+{
+    cout<<"Enter the string :";
+    string s;
+    getline(cin,s);
+    cout<<"String :"<<s<<"\n";
+    int l=s.length();
+   for(int i=0;i<l;i++)
+   {
+    if(s[i]==32)
+    s.erase(i,1);
+   }
+   cout<<"String without space :"<<s<<"\n";
+    return 0;
+}
